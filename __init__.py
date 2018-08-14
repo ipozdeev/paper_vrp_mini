3eef16d20cd779a566cd0c749504b40f4c4c244a
ipozdeev <igor.pozdeev@unisg.ch>
2018-08-14 13:10:14 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from paper_vrp_mini.core import *
+from paper_vrp_mini.covariance import *
+from paper_vrp_mini.strategy import *
+from paper_vrp_mini.helpers import *
+from paper_vrp_mini.database import ResearchData
+
+settings = {
+    "sample": ["aud", "cad", "chf", "eur", "gbp", "jpy", "nzd", "usd"],
+    "s_dt": "2006",
+    "e_dt": "2018"
+}
